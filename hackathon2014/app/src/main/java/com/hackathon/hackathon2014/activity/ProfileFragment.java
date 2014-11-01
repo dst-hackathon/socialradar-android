dd1ba3612623ddcc3a499f1d562b3aec0a6612c8
kaotangP <kaotang@gmail.com>
2014-11-01 12:46:52 +0700

    Remove Nullable annotation.

--- hackathon2014/app/src/main/java/com/hackathon/hackathon2014/activity/ProfileFragment.java
+++ hackathon2014/app/src/main/java/com/hackathon/hackathon2014/activity/ProfileFragment.java
@@ -3,7 +3,6 @@ package com.hackathon.hackathon2014.activity;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,7 +18,7 @@ public class ProfileFragment extends Fragment implements OnClickListener {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
-			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+			ViewGroup container, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.profile_fragment, container, false);
 		Button searchButton = ( Button ) view.findViewById(R.id.search);
